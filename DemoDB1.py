7a1c928e83a47047eee0be1c789560083bafe3d0
94shinshinhy <94shinshinhy@gmail.com>
2021-04-07 12:16:26 +0900

    DB2

--- DemoDB1.py
+++ DemoDB1.py
@@ -5,17 +5,26 @@ import sqlite3
 
 # 처음에는 데이터베이스파일(또는 메모리)를 생성
 con = sqlite3.connect(":memory:")
+
 # SQL 구문을 실행하는 것은 대부분 커서 객체
 cur = con.cursor()
+
 # 저장소(테이블)를 만들기 : 테이블 스키마(뼈대)
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+
 # 1건 입력
 cur.execute("insert into PhoneBook Values ('derick', '010-111');")
+
 # 입력 파마미터 처리
 # 텍스트박스(GUI, Web Page)에서 입력을 받아서 처리
 name = "gildong"
 PhoneNumber = "010-222"
-cur.execute("insert into PhoneBook Values (?,?);", (name,PhoneNumber))
+cur.execute("insert into PhoneBook Values (?, ?);", (name, PhoneNumber))
+
+# 다중의 레코드(행, row)를 입력받는 경우 : 2차원 행렬 데이터
+datalist = (("tom", "010-123"), ("dsp", "010-567"))
+cur.executemany("insert into PhoneBook Values(?, ?);", datalist)
+
 # 검색
 cur.execute("select * from PhoneBook;")
 for row in cur:
